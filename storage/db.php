2375c27ee9c82d058ff396628864db6949f263b7
aaaaa11111dr <aaaaa11111dr@users.noreply.github.com>
2026-05-22 19:27:37 +0000

    feat: 查看仓库文件
    Co-authored-by: traeagent <traeagent@users.noreply.github.com>

---
+++ storage/db.php
@@ -0,0 +1,9 @@
+<?php
+return array (
+  'host' => '127.0.0.1',
+  'port' => 3306,
+  'database' => 'wow_search',
+  'username' => 'search_user',
+  'password' => 'search_pass123',
+  'installed_at' => '2026-05-22T19:27:01+00:00',
+);
