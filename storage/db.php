ab807acb7af65568573622d216e72589a134558c
aaaaa11111dr <aaaaa11111dr@users.noreply.github.com>
2026-05-22 19:22:19 +0000

    feat: 查看仓库文件
    Co-authored-by: traeagent <traeagent@users.noreply.github.com>

--- storage/db.php
+++
@@ -1,9 +0,0 @@
-<?php
-return array (
-  'host' => '127.0.0.1',
-  'port' => 3306,
-  'database' => 'world',
-  'username' => 'root',
-  'password' => '',
-  'installed_at' => '2026-05-22T09:36:09+02:00',
-);
